373fc4fdbc2984078c89262459cf4e2e8a530d6f
peng_chao <pengchaonest@klook.com>
2015-08-14 19:52:52 +0800

    结构设计

---
+++ src/main/java/finalcode/HttpAsyncClient/HttpRequest.java
@@ -0,0 +1,7 @@
+package finalcode.HttpAsyncClient;
+
+/**
+ * Created by peng_chao_b on 15/8/14.
+ */
+public class HttpRequest {
+}
---
+++ src/main/java/finalcode/ProcessHtml/PurgeHtml.java
@@ -0,0 +1,8 @@
+package finalcode.ProcessHtml;
+
+/**
+ * Created by peng_chao_b on 15/8/14.
+ */
+public class PurgeHtml {
+
+}
